00d1a0b68e68b20d38ac63632bf80eb4683b076d
surajkumarsingh272 <201424175+surajkumarsingh272@users.noreply.github.com>
2025-10-17 15:17:26 +0530

    new add file home_screen

---
+++ lib/signup_screen.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/cupertino.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
