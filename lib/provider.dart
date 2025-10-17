bba8a508be67833ddbb18e250888d96ff5c0de63
surajkumarsingh272 <201424175+surajkumarsingh272@users.noreply.github.com>
2025-10-17 15:49:58 +0530

    add provider

---
+++ lib/provider.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/cupertino.dart';
+
+class Provider extends StatelessWidget {
+  const Provider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
